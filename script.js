d472afb6af28724a7316ee0d778c6b0b21e9ae5a
VítorHoltz <vitorholtz27@gmail.com>
2025-02-07 14:28:59 -0300

    intro-display

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ menuToggle.addEventListener('click', () => {
     menuToggle.classList.toggle('mobile-menu-toggle--active');
     mobileMenu.classList.toggle('vh-header--mobile');
 })
+
+// header change on scroll
+
+$(window).scroll(function(){
+    if ($(this).scrollTop() > 50) {
+       $('.vh-header').addClass('vh-header--scroll-mode');
+    } else {
+       $('.vh-header').removeClass('vh-header--scroll-mode');
+    }
+});
